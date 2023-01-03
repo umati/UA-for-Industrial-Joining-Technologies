da7d946ff4b65119db549d91c336d186ec96c90a
MohitAgarwal <mohit.agarwal@atlascopco.com>
2023-01-03 15:29:24 +0100

    Remvoed files to re-commit later with some changes

--- OPC_UA_Clients/NodeOPCUA_IJT_Client/Javascripts/Models/ProcessingTimesDataType.mjs
+++
@@ -1,9 +0,0 @@
-import IJTBaseModel from './IJTBaseModel.mjs';
-
-// The purpose of this class is to model the Processing times data structure
-export default class ProcessingTimesDataType extends IJTBaseModel {
-    constructor(parameters, modelManager) { 
-        super(parameters, modelManager);
-        
-    }
-}
--- OPC_UA_Clients/NodeOPCUA_IJT_Client/Javascripts/Models/ResultValueDataType.mjs
+++
@@ -1,8 +0,0 @@
-import IJTBaseModel from './IJTBaseModel.mjs';
-
-// The purpose of this class is to handle the actual subscription or reading of a value and via socketIO send the result to the webpage
-export default class ResultValueDataType extends IJTBaseModel {
-    constructor(parameters, modelManager) { 
-        super(parameters, modelManager);
-    }
-}
