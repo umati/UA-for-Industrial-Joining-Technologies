8806eafceb0f1f99cf6e89eb3610fa98a3e608c5
Joakim Gustafsson <joakim.gustafsson@atlascopco.com>
2023-03-22 18:01:19 +0100

    improving sockethandling

--- OPC_UA_Clients/NodeOPCUA_IJT_Client/Javascripts/Client/NodeOPCUAInterface.mjs
+++ OPC_UA_Clients/NodeOPCUA_IJT_Client/Javascripts/Client/NodeOPCUAInterface.mjs
@@ -5,6 +5,7 @@ import {
   AttributeIds,
   OPCUAClient,
   promoteOpaqueStructure,
+  makeBrowsePath,
   Variant,
   NumericRange,
   TimestampsToReturn,
@@ -27,15 +28,19 @@ export default class NodeOPCUAInterface {
 
     io.on('connection', (socket) => {
       //----------------------------------------------------------------------------------------- SOCKET: Interaction with items item
-      socket.on('subscribe item', msg => {
-        this.addMonitor(msg);
+      socket.on('subscribe item', (callid, msg) => {
+        this.addMonitor(callid, msg);
       });
-      socket.on('read', msg => {
-        this.read(msg);
+      socket.on('read', (callid, msg) => {
+        this.read(callid, msg);
       });
 
-      socket.on('browse', (nodeId, details) => {
-        this.browse(nodeId, details)
+      socket.on('browse', (callid, nodeId, details) => {
+        this.browse(callid,nodeId, details)
+      });
+
+      socket.on('pathtoid', (callid, nodeId, path) => {
+        this.translateBrowsePath(callid, nodeId, path) 
       });
 
 
@@ -85,26 +90,22 @@ export default class NodeOPCUAInterface {
           if (!err) {
             theSession = session;
             thisContainer.session = session;
-       
+
 
             io.emit('session established');
           }
           callback(err);
         });
-      }, /*
-      function(callback) { //Namespaces
-        
-        thisContainer.session.readNamespaceArray((namespaces)=> {
-          console.log("The server do not expose the IJT namespace");
-          /*
-         const nsIJT = namespaces.indexOf("http://opcfoundation.org/UA/IJT/");
-         if (nsIJT < 0) {
-             console.log("The server do not expose the IJT namespace");
-             return;
-         } 
-             //this.displayFunction("Creating namsepaces ");
-        }) 
-      }*/
+      },
+      function (callback) { // Namespaces
+        /*
+        thisContainer.session.readNamespaceArray((err, namespaces) => {
+          console.log("Handling NameSpaces");
+          // console.log(namespaces);
+
+          io.emit('namespaces', namespaces);
+        }) */
+      }
 
 
     ],
@@ -121,10 +122,52 @@ export default class NodeOPCUAInterface {
   }
 
   // Read some value
-  read(nodeId) {
+  read(callid, nodeId) {
     (async () => {
       try {
+        console.log('Entered read :'+nodeId);
+        const dataValue = await this.session.read({
+          nodeId,
+          attributeId: AttributeIds.Value,
+        });
+
+        console.log('dataValue1 ' + dataValue.toString());
+
+        const result = dataValue.value.value;
+        if (result && result.resultContent) {
+          await promoteOpaqueStructure(this.session, [{ value: result.resultContent }]);
+        }
+
+        console.log('dataValue ' + dataValue.toString());
+
+        this.io.emit('readresult', { 'callid': callid, 'dataValue': dataValue, 'stringValue': dataValue.toString(), 'nodeid': nodeId });
+        return dataValue;
+
+      } catch (err) {
+        this.displayFunction("Node.js OPC UA client error (reading): " + err.message);  // Display the error message first
+        this.io.emit('error message', err.toString(), 'read');
+        //this.displayFunction(err);                                                      // (Then for debug purposes display all of it)
+      }
+    })()
+  };
+
+  // Under construction
+  translateBrowsePath(callid, nodeId, path) {
+    (async () => {
+      try {
+
+        const bpr2 = await session.translateBrowsePath(
+          makeBrowsePath(nodeId, `/${nsIJT}:ResultManagement/${nsIJT}:Results`));
+
+        if (bpr2.statusCode !== StatusCodes.Good) {
+          console.log("Cannot find the ResultManagement object ");
+          return;
+        }
+        const resultsNodeId = bpr2.targets[0].targetId;
+        this.io.emit('pathtoidresult', { 'callid': callid, 'nodeid': resultsNodeId});
+        /*
         //console.log('Entered read');
+        
         const dataValue = await this.session.read({
           nodeId,
           attributeId: AttributeIds.Value,
@@ -136,16 +179,15 @@ export default class NodeOPCUAInterface {
         }
         //console.log('dataValue ' + dataValue.toString());
         this.io.emit('readresult', { 'path': nodeId, 'dataValue': dataValue, 'stringValue': dataValue.toString() });
-        return dataValue;
+        return dataValue; */
 
       } catch (err) {
-        this.displayFunction("Node.js OPC UA client error (reading): " + err.message);  // Display the error message first
-        this.io.emit('error message', err.toString(), 'read');
+        this.displayFunction("Node.js OPC UA client error (translateBrowsePath): " + err.message);  // Display the error message first
+        this.io.emit('error message', err.toString(), 'translateBrowsePath');
         //this.displayFunction(err);                                                      // (Then for debug purposes display all of it)
       }
     })()
   };
-
 
   closeConnection(callback) {
     console.log("Closing");
@@ -162,7 +204,7 @@ export default class NodeOPCUAInterface {
     console.log("Client disconnected");
   }
 
-  browse(nodeId, details = false) {
+  browse(callid, nodeId, details = false) {
     (async () => {
       try {
         let io = this.io;
@@ -187,8 +229,9 @@ export default class NodeOPCUAInterface {
               // console.log(reference.browseName);
               //});
               io.emit('browseresult', {
-                'callernodeid': nodeId,
+                'callid': callid,
                 'browseresult': browse_result,
+                'nodeid': nodeId
               });
             };
           }
@@ -202,7 +245,7 @@ export default class NodeOPCUAInterface {
   }
 
   // Subscribe to some value.      Not tested    Total Rewrite???
-  addMonitor(path) {
+  addMonitor(callid, path) {
     let itemToMonitor = {
       nodeId: path,
       attributeId: this.AttributeIds.Value
@@ -230,128 +273,4 @@ export default class NodeOPCUAInterface {
       }
     })();
   }
-
-
-  /*
-    testfunc() {
-      console.log('Entered test function1');
-      const
-        endpointUrl =
-          "opc.tcp://10.46.19.106:40451";   // Simulator
-      //"opc.tcp://localhost:40451";        // Local application
-  
-      const
-        nodeId =
-          "ns=1;s=/ObjectsFolder/TighteningSystem_AtlasCopco/ResultManagement/Results/Result";  // Simulator
-      //"ns=1;s=/ObjectsFolder/TighteningSystem/ResultManagement/Results/Result";               // Local application
-  
-  
-      (async () => {
-  
-        console.log('Entered test function4');
-        const
-          client =
-            OPCUAClient.create({
-              endpointMustExist:
-                false,
-            });
-        client.on("backoff", () => {
-          console.log("still trying to connect  to ", endpointUrl);
-        });
-  
-        console.log('Entered test function3');
-  
-  
-        await client.withSessionAsync(endpointUrl, async (session) => {
-  
-          console.log('Entered test function2');
-          const dataValue = await session.read({
-            nodeId,
-            attributeId: AttributeIds.Value,
-          });
-  
-          const result = dataValue.value.value;
-  
-          console.log('BBBBBBBBBBBBBBBBB ');
-          if (result && result.resultContent) {
-            await promoteOpaqueStructure(session, [{ value: result.resultContent }]);
-          }
-          //console.log('AAAAAAAAAAAAAAAAA ' + dataValue.toString());
-          return dataValue;
-        });
-  
-  
-        console.log('CCCCCCCCCCCCCCCCC ');
-        /*
-              await
-                client.withSessionAsync(endpointUrl,
-                  async (session) => {
-                    const
-                      d =
-                        await session.read({
-                          nodeId,
-                          attributeId:
-                            AttributeIds.Value,
-                          indexRange:
-                            new NumericRange("185492:5428372"),
-                        });
-                    console.log('Result: ' + d.toString());
-                  }); 
-  
-  
-      })();
-    }  */
-
 }
-
-
-/*  FROM DEMO: Subscribe and Monitor
-    // step 5: install a subscription and monitored item
-    //
-    // -----------------------------------------
-    // create subscription
-    function(callback) {
-
-      theSession.createSubscription2({
-          requestedPublishingInterval: 1000,
-          requestedLifetimeCount: 1000,
-          requestedMaxKeepAliveCount: 20,
-          maxNotificationsPerPublish: 10,
-          publishingEnabled: true,
-          priority: 10
-      }, function(err, subscription) {
-          if (err) { return callback(err); }
-          theSubscription = subscription;
-
-          theSubscription.on("keepalive", function() {
-              console.log("keepalive");
-          }).on("terminated", function() {
-          });
-          callback();
-      });
-
-  }, function(callback) {
-      // install monitored item
-      //
-      theSubscription.monitor({
-          nodeId,
-          attributeId: AttributeIds.Value
-      },
-          {
-              samplingInterval: 100,
-              discardOldest: true,
-              queueSize: 10
-          }, TimestampsToReturn.Both,
-          (err, monitoredItem) => {
-              console.log("-------------------------------------");
-              monitoredItem
-                  .on("changed", function(value) {
-                      console.log(" New Value = ", value.toString());
-                  })
-                  .on("err", (err) => {
-                      console.log("MonitoredItem err =", err.message);
-                  });
-              callback(err);
-
-          });
-          */
