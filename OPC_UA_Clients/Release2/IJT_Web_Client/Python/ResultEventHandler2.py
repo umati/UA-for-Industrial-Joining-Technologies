7187fd07a9abf95523b18efad36b59112335e5e1
MohitAgarwal <mohit.agarwal@atlascopco.com>
2024-11-05 16:40:55 +0100

    Fixed some exceptions in the Result Handler - WIP - It is NOT working yet.

--- OPC_UA_Clients/Release2/IJT_Web_Client/Python/ResultEventHandler2.py
+++ OPC_UA_Clients/Release2/IJT_Web_Client/Python/ResultEventHandler2.py
@@ -1,20 +1,19 @@
-
 import json
-from threading import Thread
 import asyncio
 import queue
+from threading import Thread
 from Python.Serialize import serializeClassInstance, serializeValue
 
-class Short():
+class Short:
     """
-    Supportclass to contain just the part of a resultevent that should be transfered to the webpage
+    Support class to contain just the part of a result event that should be transferred to the webpage
     """
     def __init__(self, eventType, result, message):
         self.EventType = eventType
         self.Result = result
         self.Message = message
 
-class ResultEventHandler():
+class ResultEventHandler:
     """
     Subscription Handler. To receive events from server for a subscription
     data_change and event methods are called directly from receiving thread.
@@ -25,55 +24,45 @@ class ResultEventHandler():
         self.websocket = websocket
         self.server_url = server_url
         self.queue = queue.Queue()
-        print("xxxxxxxxxxx")
-        self.handleQueue()
- 
+        self.loop = asyncio.get_event_loop()
+        self.loop.create_task(self.handleQueue())
+
     async def handleQueue(self):
-      print("handleQueue")
-      item = self.queue.get()
-      if item is None:
-            thread = Thread(target=self.handleQueue)
-            thread.start()
-            return
-      print('----------------------- Processing 1')
-      await self.websocket.send(item) 
-      print('----------------------- Processing 2')
-      thread = Thread(target=self.handleQueue)
-      thread.start()
+        while True:
+            item = await self.loop.run_in_executor(None, self.queue.get)
+            if item is None:
+                break
+            await self.websocket.send(item)
 
     async def threaded_websocket(self, tmp):
         stage = "1"
         try:
-          arg = str(serializeValue(tmp))
-          returnValue = {
-             "command" : "event",
-             "endpoint": self.server_url,
-             "data": arg,
-          }
-          stage = "2"
-          tmp2 = json.dumps(returnValue)
-          stage = "3"
-          print("start " + tmp.Message.Text)
-          self.queue.put(tmp)
-          stage = "4"
-          print("end   " + tmp.Message.Text)
+            arg = str(serializeValue(tmp))
+            returnValue = {
+                "command": "event",
+                "endpoint": self.server_url,
+                "data": arg,
+            }
+            stage = "2"
+            tmp2 = json.dumps(returnValue)
+            stage = "3"
+            print("start " + tmp.Message.Text)
+            self.queue.put(tmp2)
+            stage = "4"
+            print("end   " + tmp.Message.Text)
         except Exception as e:
-            print("--- TW Exception: " +stage + " --- " + str(e))
-
+            print("--- TW Exception: " + stage + " --- " + str(e))
 
     def wrap_async_func(self, arg):
-        # temp = serializeValue(arg)
-        asyncio.run(self.threaded_websocket(arg))
+        asyncio.run_coroutine_threadsafe(self.threaded_websocket(arg), self.loop)
 
     def event_notification(self, event):
         print("RESULT EVENT RECEIVED: " + event.Message.Text)
         
         filteredEvent = Short(event.EventType, event.Result, event.Message)
 
-        # Eventhandlers should be quick and non networked so sending the response
-        # to the webpage needs to be done asyncronously via a separate thread.
-        # Therefore we esure we send JUST the result part (and the eventType for identification, and the message)
+        # Event handlers should be quick and non-networked so sending the response
+        # to the webpage needs to be done asynchronously via a separate thread.
+        # Therefore we ensure we send JUST the result part (and the eventType for identification, and the message)
 
-        thread = Thread(target = self.wrap_async_func, args = (filteredEvent, ))
-        thread.start()
-
+        self.wrap_async_func(filteredEvent)
