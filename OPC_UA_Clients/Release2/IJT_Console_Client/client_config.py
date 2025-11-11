ad72f18d1a2c07e007f76eeb0bd948dee5de9145
MohitAgarwal <mohit.agarwal@atlascopco.com>
2025-11-11 11:57:27 +0100

    Changed default IP to localhost

--- OPC_UA_Clients/Release2/IJT_Console_Client/client_config.py
+++ OPC_UA_Clients/Release2/IJT_Console_Client/client_config.py
@@ -4,7 +4,7 @@ import re
 URL_PATTERN = re.compile(r"opc\.tcp://[a-zA-Z0-9\.\-]+:\d+")
 
 # Default OPC UA server URL (used if --url is not provided)
-SERVER_URL = "opc.tcp://10.46.19.106:40451"
+SERVER_URL = "opc.tcp://localhost:40451"
 
 # Enable result file logging (project-level constant)
 ENABLE_RESULT_FILE_LOGGING = True
