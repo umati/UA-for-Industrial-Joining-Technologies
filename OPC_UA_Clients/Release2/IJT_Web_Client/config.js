9a3b0d0e60e92a8989a6a9232c751093e61ad75d
Joakim Gustafsson <joakim.gustafsson@atlascopco.com>
2026-03-11 11:56:18 +0100

    handling ::1 hostname

--- OPC_UA_Clients/Release2/IJT_Web_Client/config.js
+++ OPC_UA_Clients/Release2/IJT_Web_Client/config.js
@@ -1,5 +1,7 @@
 window.APP_CONFIG = {
-  WS_HOST: window.location.hostname,
+  WS_HOST: (window.location.hostname === 'localhost' || window.location.hostname === '::1')
+    ? '127.0.0.1'
+    : window.location.hostname,
   WS_PORT: '8001',
   WS_PROTOCOL: window.location.protocol === 'https:' ? 'wss:' : 'ws:',
   getWebSocketUrl: function () {
